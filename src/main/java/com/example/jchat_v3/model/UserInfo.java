c4966830a10d12bba04f49582c76c5508a95d019
lanhhv84 <hvlpro98@gmail.com>
2019-03-12 23:55:37 +0700

    Refining UserInfo

--- src/main/java/com/example/jchat_v3/model/UserInfo.java
+++ src/main/java/com/example/jchat_v3/model/UserInfo.java
@@ -1,5 +1,7 @@
 package com.example.jchat_v3.model;
 
+import java.util.Objects;
+
 public class UserInfo {
     private int id;
     private String nickName;
@@ -18,5 +20,28 @@ public class UserInfo {
 
     public void setNickName(String nickName) {
         this.nickName = nickName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "id=" + id +
+                ", nickName='" + nickName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserInfo userInfo = (UserInfo) o;
+        return id == userInfo.id &&
+                Objects.equals(nickName, userInfo.nickName);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, nickName);
     }
 }
